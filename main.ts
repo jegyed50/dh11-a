6c3a95ebaa8a59fcae13371119d693b076fe53e7
Egyed József <jegyed50@gmail.com>
2023-10-10 07:54:56 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let A = 0
 basic.forever(function () {
-	
+    A += 1
+    dht11_dht22.queryData(
+    DHTtype.DHT11,
+    DigitalPin.P0,
+    true,
+    true,
+    true
+    )
+    basic.pause(2000)
+    if (dht11_dht22.sensorrResponding()) {
+        basic.showString("" + (dht11_dht22.readData(dataType.humidity)))
+        if (dht11_dht22.readDataSuccessful()) {
+            basic.showString("" + (dht11_dht22.readData(dataType.humidity)))
+        }
+    }
+    serial.writeValue("A", A)
 })
